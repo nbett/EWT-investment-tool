a9cc1352b673da5233d6326b4e37980f11a20f75
nbett <34234185+nbett@users.noreply.github.com>
2025-10-21 14:52:20 +0100

    Create __init__.py

---
+++ src/ewmc/__init__.py
@@ -0,0 +1,5 @@
+from .zigzag import zigzag_atr
+from .wave_labeler import build_counts
+from .distributions import WaveDistributions
+from .simulator import MonteCarloEW
+from .visualize import plot_targets, plot_time_to_target, plot_ote_heatmap
